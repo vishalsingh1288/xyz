e0db8a41f55ff44f1d6431550e6f7b4285aa6d96
vishalsingh1288 <vishalsingh1288@gmail.com>
2025-06-20 17:51:59 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,4 @@
+import random
+fruit_names = ["Apple", "Apricot", "Banana", "Blackberry", " Blueberry", "Cantaloupe", "Cherry", "Date", "Dragon Fruit", "Fig", "Grape", "Grapefruit", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear"]
+weekly_fruits=random.sample(fruit_names,7)
+print(weekly_fruits)

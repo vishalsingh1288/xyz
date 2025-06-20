d34d630d61f83e1c99ef67ed911cefed47915d1d
vishalsingh1288 <vishalsingh1288@gmail.com>
2025-06-20 18:00:07 +0800

    test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+import streamlit as st
 import random
-fruit_names = ["Apple", "Apricot", "Banana", "Blackberry", " Blueberry", "Cantaloupe", "Cherry", "Date", "Dragon Fruit", "Fig", "Grape", "Grapefruit", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear"]
-weekly_fruits=random.sample(fruit_names,7)
-print(weekly_fruits)
+
+fruit_names = ["Apple", "Apricot", "Banana", "Blackberry", "Blueberry", "Cantaloupe", "Cherry", "Date", "Dragon Fruit", "Fig", "Grape", "Grapefruit", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Papaya", "Peach", "Pear"]
+
+weekly_fruits = random.sample(fruit_names, 7)
+
+st.title("Weekly Fruit Picker")
+st.write("Here are 7 fruits picked randomly for this week:")
+
+for fruit in weekly_fruits:
+    st.write(f"- {fruit}")
